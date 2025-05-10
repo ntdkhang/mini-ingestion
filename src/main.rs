0f4f1706e253cc2c5c98fc3c249d9074ba904200
Khang <ntdkhang143@gmail.com>
2025-05-10 09:06:04 +0700

    send event to clickhouse

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
+use clickhouse::{Client, Row};
 use rdkafka::config::RDKafkaLogLevel;
 use rdkafka::consumer::{CommitMode, Consumer, StreamConsumer};
 use rdkafka::{ClientConfig, Message};
-use rdkafka::message::Headers;
-// use serde_json::{json, Value};
+// use rdkafka::message::Headers;
+use serde::{Serialize, Deserialize};
 
+#[derive(Row, Serialize, Deserialize)]
+struct MyRow {
+    user_id: String,
+    event: String,
+}
 
-async fn consume(brokers: &str, group_id: &str, topics: &[&str]) {
+#[derive(Serialize, Deserialize)]
+struct MyMessage {
+}
+
+async fn consume_to_clickhouse(client: &Client, brokers: &str, group_id: &str, topics: &[&str]) {
+    let mut inserter = client.inserter("test")
+        .unwrap()
+        .with_max_rows(100);
+
     let consumer: StreamConsumer = ClientConfig::new()
         .set("group.id", group_id)
         .set("bootstrap.servers", brokers)
@@ -36,12 +50,15 @@ async fn consume(brokers: &str, group_id: &str, topics: &[&str]) {
                 };
                 println!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
                       m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
+                let row: MyRow = serde_json::from_str(payload).expect("Failed to Deserialize payload");
+                let _ = inserter.write(&row);
+                inserter.commit().await.unwrap();
 
-                if let Some(headers) = m.headers() {
-                    for header in headers.iter() {
-                        println!("  Header {:#?}: {:?}", header.key, header.value);
-                    }
-                }
+                // if let Some(headers) = m.headers() {
+                //     for header in headers.iter() {
+                //         println!("  Header {:#?}: {:?}", header.key, header.value);
+                //     }
+                // }
                 consumer.commit_message(&m, CommitMode::Async).unwrap();
             }
         }
@@ -51,6 +68,10 @@ async fn consume(brokers: &str, group_id: &str, topics: &[&str]) {
 
 #[tokio::main]
 async fn main() {
-    consume("localhost:9094", "my_group", &["quickstart-events"]).await
+    let client = Client::default()
+        // .with_url("http://localhost:8123")
+        .with_database("test");
+
+    consume_to_clickhouse(&client, "localhost:9094", "my_group", &["quickstart-events"]).await;
 }
 
